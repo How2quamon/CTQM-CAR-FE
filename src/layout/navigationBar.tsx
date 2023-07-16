4d712867759becd3b6cec1995d601562a0667bd2
fontaine07 <tramnn.0702@gmail.com>
2023-07-17 01:11:53 +0700

    modify nav + product details page

--- src/layout/navigationBar.tsx
+++ src/layout/navigationBar.tsx
@@ -6,9 +6,13 @@ import { Input, Space } from 'antd';
 
 const NavBar = () => {
   const [showSideBar, setSideBar] = useState(false);
-
   const handleNav = () => {
     setSideBar(!showSideBar);
+  };
+
+  const [changeSearchColor, setSearchColor] = useState(false);
+  const handleSearchBar = () => {
+    setSearchColor(!changeSearchColor);
   };
   // const { Search } = Input;
   // const onSearch = (value: string) => console.log(value);
@@ -16,92 +20,86 @@ const NavBar = () => {
 
   return (
     <div className="sticky top-0 z-10 bg-stone-950 text-slate-50">
-      <div className="flex justify-between items-center font-bold px-12 p-4 text-sm ">
-        <div>
-          <a href="#">
-            <img className="h-5" src={logo} alt="CTQM logo" />
-          </a>
-        </div>
-        <div className="hidden lg:inline">
-          <ul className="flex justify-center hover:cursor-pointer">
-            <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="#">Mayback</a>
-            </li>
-            <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="#">Mercedes</a>
-            </li>
-            <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="#">AMG</a>
-            </li>
-            <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="#">Model Y</a>
-            </li>
-            <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="#">Solar Roof</a>
-            </li>
-            <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="#">Solar Panels</a>
-            </li>
-          </ul>
-        </div>
-        <div className="hidden lg:inline">
-          <ul className="flex justify-center hover:cursor-pointer">
-            {/* <li className="py-2 px-4 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="#">Cart</a>
-            </li> */}
-            <li className="flex justify-between items-center py-2 px-4 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <UserOutlined style={{ fontSize: '18px', paddingRight: '4px' }} rev={undefined} />
-              <a href="#">Account</a>
-            </li>
-            <li onClick={handleNav} className="py-2 px-4 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              Menu
-            </li>
-          </ul>
-        </div>
-        <div className="lg:hidden">
-          <button onClick={handleNav} className="inline-flex items-center rounded-md py-2 px-4 p-2 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10 transition duration-150 ease-out hover:ease-in">
-            Menu
-          </button>
-        </div>
-
-        <div
-          className={
-            showSideBar
-              ? "bg-stone-950 absolute right-0 top-0 w-80 max-h-screen z-1001 ease-in-out duration-300 backdrop-blur-sm"
-              : "fixed right-[-100%] "
-          }
-        >
-          <div className="flex justify-end pr-8 pt-7">
-            <CloseOutlined onClick={handleNav} className="rounded p-2 hover:bg-slate-700 transition duration-150 ease-out hover:ease-in" style={{ fontSize: '18px' }} rev={undefined} />
+      <div className="max-w-7xl- mx-auto px-4 sm:px-6 lg:px-8 py-1 md:py-4">
+        <div className="flex justify-between items-center font-bold text-sm md:justify-start">
+          
+            <a href="#">
+              <img className="h-5" src={logo} alt="CTQM logo" />
+            </a>
+          
+          <div className="hidden lg:inline md:flex space-x-3 flex-1 lg:ml-8">
+            <ul className="flex justify-start ">
+              <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in hover:cursor-pointer">
+                <a href="#">Mayback</a>
+              </li>
+              <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in hover:cursor-pointer">
+                <a href="#">Mercedes</a>
+              </li>
+              <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in hover:cursor-pointer">
+                <a href="#">AMG</a>
+              </li>
+              <li className="py-2 px-4 mx-1 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in hover:cursor-pointer">
+                <a href="#">Model Y</a>
+              </li>
+            </ul>
           </div>
-          <ul className="h-screen pt-8 px-6 bg-stone-950 ">
-
-            <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in ">
-              <a href="https://www.tesla.com/inventory/new/m3">
-                Existing Inventory
-              </a>
-            </li>
-            <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="https://www.tesla.com/inventory/used/m3">
-                Used Inventory
-              </a>
-            </li>
-            <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="https://www.tesla.com/tradein">Trade-In</a>
-            </li>
-            <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="https://www.tesla.com/drive">Test Drive</a>
-            </li>
-            <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
-              <a href="https://www.tesla.com/insurance">Insurance</a>
-            </li>
-            <li className="py-5 pl-3 inline-block align-middle">
-              <div className="flex justify-between items-center rounded-md bg-slate-700">
-                <input type={"search"} placeholder="Search..." className="pl-3 bg-transparent w-full text-white text-sm focus:outline-none" />
-                <SearchOutlined className="px-2 py-1 text-white text-lg block float-right cursor-pointer mr-2" rev={undefined} />
+          <div className="flex justify-between items-center space-x-4">
+            <div className="relative hidden md:block">
+              
+                <div onClick={handleSearchBar} className={changeSearchColor ? "flex items-center border-b border-b-gray-700 bg-slate-700 rounded-lg" : "flex items-center border-b border-b-gray-700"}>                
+                  <SearchOutlined className="pl-2 pt-1 text-white text-lg block float-left cursor-pointer mr-2" rev={undefined} />
+                  <input type={"search"} placeholder="Search..." className="bg-transparent w-full text-white text-sm focus:outline-none focus:shadow-inner leading-none" />
+                </div>
+              
+             
+            </div>
+            <div className="flex items-center py-2 px-4 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
+                <UserOutlined style={{ fontSize: '18px', paddingRight: '4px' }} rev={undefined} />
+                <a href="#">Account</a>
+            </div>
+              <div onClick={handleNav} className="py-2 px-4 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in hover:cursor-pointer">
+                Menu
               </div>
-            </li>
-          </ul>
+          </div>
+          <div className="lg:hidden">
+            <button onClick={handleNav} className="inline-flex items-center rounded-md py-2 px-4 p-2 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10 transition duration-150 ease-out hover:ease-in">
+              Menu
+            </button>
+          </div>
+
+          <div
+            className={
+              showSideBar
+                ? "bg-stone-950 absolute right-0 top-0 w-80 max-h-screen z-1001 ease-in-out duration-300 backdrop-blur-sm"
+                : "fixed right-[-100%] "
+            }
+          >
+            <div className="flex justify-end pr-8 pt-7">
+              <CloseOutlined onClick={handleNav} className="rounded p-2 hover:bg-slate-700 transition duration-150 ease-out hover:ease-in" style={{ fontSize: '18px' }} rev={undefined} />
+            </div>
+            <ul className="h-screen pt-8 px-6 bg-stone-950 ">
+
+              <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in ">
+                <a href="https://www.tesla.com/inventory/new/m3">
+                  Existing Inventory
+                </a>
+              </li>
+              <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
+                <a href="https://www.tesla.com/inventory/used/m3">
+                  Used Inventory
+                </a>
+              </li>
+              <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
+                <a href="https://www.tesla.com/tradein">Trade-In</a>
+              </li>
+              <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
+                <a href="https://www.tesla.com/drive">Test Drive</a>
+              </li>
+              <li className="py-5 pl-3 hover:rounded hover:bg-slate-700 transition duration-150 ease-out hover:ease-in">
+                <a href="https://www.tesla.com/insurance">Insurance</a>
+              </li>            
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/products/productDetails/productDetails.tsx
+++ src/pages/products/productDetails/productDetails.tsx
@@ -1,23 +1,160 @@
-import { Carousel } from '@material-tailwind/react'
-import React from 'react'
+import React, { useState } from 'react'
+import useTitle from 'src/hooks/useTitle';
+import NavBar from 'src/layout/navigationBar';
+import { Select } from 'antd';
+import { Card } from 'antd';
+import Footer from 'src/layout/Footer';
 
-const ProductDetails = () => (
-    <div>
-        <Carousel transition={{ duration: 2 }} className="rounded-xl">
-            <img
-                src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                alt="image 1"
-                className="h-48 w-96 object-cover" />
-            <img
-                src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                alt="image 2"
-                className="h-48 w-96 object-cover" />
-            <img
-                src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-                alt="image 3"
-                className="h-48 w-96 object-cover" />
-        </Carousel>
-    </div>
-)
+const ProductVariants = [
+    {
+      color: 'Emerald Green metallic',
+    },
+    {
+      color: 'Magnetite Black metallic',
+    },
+    {
+      color: 'Ocean Blue metallic',
+    }
+  ]
+
+const { Meta } = Card;
+
+const ProductDetails: React.FC = () => {
+    useTitle("Chi tiết sản phẩm");
+    const [images, setImages] = useState({
+        img1 : "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        img2 : "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        img3 : "https://images.unsplash.com/photo-1511919884226-fd3cad34687c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        img4 : "https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+    })
+    const [activeImg, setActiveImage] = useState(images.img1)
+
+    const [amount, setAmount] = useState(1);
+
+    const [ProductVariant, setProductVariant] = useState(ProductVariants[0])
+
+    const handleChange = (value: { value: string; label: React.ReactNode }) => {
+        console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
+      };
+
+    return (
+        <React.Fragment>
+            <NavBar/>
+            <main>
+                <div className='flex flex-col justify-center items-center min-h-screen'>
+                <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:place-items-center lg:py-20">
+                        <article>
+                            <img src={activeImg} alt="" className="w-full h-full aspect-square object-cover rounded-xl"/>
+                            <div className='hidden lg:flex items-center justify-center gap-5 flex-wrap mt-5'>
+                                <img src={images.img1} alt="" className='w-32 h-32 object-cover rounded-lg mb-4 cursor-pointer' onClick={() => setActiveImage(images.img1)}/>
+                                <img src={images.img2} alt="" className='w-32 h-32 object-cover rounded-lg mb-4 cursor-pointer' onClick={() => setActiveImage(images.img2)}/>
+                                <img src={images.img3} alt="" className='w-32 h-32 object-cover rounded-lg mb-4 cursor-pointer' onClick={() => setActiveImage(images.img3)}/>
+                                <img src={images.img4} alt="" className='w-32 h-32 object-cover rounded-lg mb-4 cursor-pointer' onClick={() => setActiveImage(images.img4)}/>
+                            </div> 
+                        </article>
+                         {/* ABOUT */}
+                        <article className='space-x-4'>
+                            <div className='space-x-4 mb-4'>
+                                <span className='mx-4 mb-2 text-sky-600 font-semibold'>Mercedes</span>
+                                <h1 className='text-3xl font-bold'>Mercedes-AMG G63 2019</h1>
+                            </div>
+                            <p className='my-2 text-gray-700 leading-7'>
+                            With 577 handcrafted horses, the AMG G-63 is a legend raised to a higher power for a new era. Advanced luxury, unwavering confidence, and extensive individualization let you create a G that's at ease in any corner of the world.
+                            </p>
+                            <h6 className='my-2 text-2xl font-semibold'>12.000.000.000 VND</h6>
+                            <div className='my-3 w-72'>
+                                <h6 className='mb-2'>Color(s)</h6>
+                                    <Select
+                                        labelInValue
+                                        defaultValue={{ value: 'Emerald Green metallic', label: 'Emerald Green metallic' }}
+                                        style={{height: '150%'}}
+                                        onChange={handleChange}
+                                        options={[
+                                        {
+                                            value: 'Emerald Green metallic',
+                                            label: 'Emerald Green metallic',
+                                        },
+                                        {
+                                            value: 'Magnetite Black metallic',
+                                            label: 'Magnetite Black metallic',
+                                        },
+                                        ]}
+                                    />
+                            </div>
+                           
+                            <div className='items-center'>
+                                <div className="my-4 w-1/4 flex justify-between items-center border border-gray-200 rounded">
+                                    <button                                    
+                                    className="w-10 h-10 leading-5 text-gray-600 transition hover:opacity-75" onClick={() => setAmount((prev) => prev - 1)}>
+                                    -
+                                    </button>
+                                    <span className="text-center items-center">{amount}</span>
+                                    <button
+                                    type="button"
+                                    className="w-10 h-10 leading-5 text-gray-600 transition hover:opacity-75" onClick={() => setAmount((prev) => prev + 1)}>
+                                    +
+                                    </button>
+                                </div>
+                                <div className='items-center flex-col'>
+                                    <button className='my-2 w-3/5  border border-zinc-600 hover:bg-slate-50 text-black font-semibold py-3 px-6 rounded-xl transition ease-in-out duration-300 hover:ease-in'>Add to cart</button>
+                                    <button className='w-3/5 bg-slate-800 border border-zinc-600 hover:bg-slate-700 text-white font-semibold py-3 px-6 rounded-xl transition ease-in-out duration-300 hover:ease-in'>Buy it now</button>
+                                </div>                    
+                            </div>
+                        </article>
+                </section>
+                <section className='mx-20'>
+                    <h1 className='text-[30px] font-bold text-center'>Related Products</h1>
+                    <div className='my-4 flex flex-col sm:flex-row items-center justify-evenly flex-wrap gap-5'>
+                    <Card
+                        hoverable
+                        style={{ width: 300 }}
+                        cover={<img alt="example" src="https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />}
+                    >
+                        <Meta title="Mercedes-AMG GT" description="7.000.000.000 VND" />
+                    </Card>
+                    <Card
+                        hoverable
+                        style={{ width: 300 }}
+                        cover={<img alt="example" src="https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />}
+                    >
+                        <Meta title="Mercedes-AMG GT" description="7.000.000.000 VND" />
+                    </Card>
+                    <Card
+                        hoverable
+                        style={{ width: 300 }}
+                        cover={<img alt="example" src="https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />}
+                    >
+                        <Meta title="Mercedes-AMG GT" description="7.000.000.000 VND" />
+                    </Card>
+                    <Card
+                        hoverable
+                        style={{ width: 300 }}
+                        cover={<img alt="example" src="https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />}
+                    >
+                        <Meta title="Mercedes-AMG GT" description="7.000.000.000 VND" />
+                    </Card>
+                    <Card
+                        hoverable
+                        style={{ width: 300 }}
+                        cover={<img alt="example" src="https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />}
+                    >
+                        <Meta title="Mercedes-AMG GT" description="7.000.000.000 VND" />
+                    </Card>
+                    <Card
+                        hoverable
+                        style={{ width: 300 }}
+                        cover={<img alt="example" src="https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />}
+                    >
+                        <Meta title="Mercedes-AMG GT" description="7.000.000.000 VND" />
+                    </Card>
+                    </div>
+                </section>
+                </div>
+            </main>
+            <Footer/>
+        </React.Fragment>
+        
+    )
+}
 
 export default ProductDetails
