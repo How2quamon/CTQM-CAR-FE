2fe406698e149487aec0259fee41937bb61302a0
fontaine07 <tramnn.0702@gmail.com>
2023-08-18 22:33:15 +0700

    customer management #1

--- src/pages/manage/customermanage/customermanage.tsx
+++ src/pages/manage/customermanage/customermanage.tsx
@@ -1,91 +1,166 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
+import {
+  DeleteOutlined,
+  EditOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+import {
+  Button,
+  Card,
+  Dropdown,
+  Form,
+  Menu,
+  Pagination,
+  Popconfirm,
+  Table,
+  message,
+  notification,
+} from "antd";
+import React, { useEffect, useState } from "react";
+import useTitle from "src/hooks/useTitle";
 import NavBar from "src/layout/navigationBar";
+import { showNotificationError } from "src/utils/notification";
+import { ctqmService } from "../../../services/ctqm.services";
+import { CustomerDTO } from "../../../share/dtos/service-proxies-dtos";
+import { columns } from "./columns";
+import modal from "antd/es/modal";
+import usePopup from "src/hooks/usePopup";
+import AddResourcePlanPopup from "./Update";
+import { Link } from "react-router-dom";
 import Footer from "src/layout/Footer";
-import useTitle from 'src/hooks/useTitle';
+// eslint-disable-next-line react-hooks/rules-of-hooks
 
-const { Column, ColumnGroup } = Table;
+const title = "Customer Management";
 
-interface DataType {
-    key: React.Key;
-    firstName: string;
-    lastName: string;
-    age: number;
-    address: string;
-    tags: string[];
-}
+export default function ProductManagement() {
+  useTitle(title);
+  const [data, setData] = useState<any>({
+    items: [],
+    totalCount: 0,
+  });
+  const {
+    show: showUpdatePopup,
+    hidden: hiddenUpdatePopup,
+    popupComponent: updatePopup,
+  } = usePopup();
+  const [customers, setCustomers] = useState<CustomerDTO[]>([]);
+  const [loading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {
+    getCustomer();
+  }, []);
 
-const data: DataType[] = [
-    {
-        key: '1',
-        firstName: 'John',
-        lastName: 'Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-    },
-    {
-        key: '2',
-        firstName: 'Jim',
-        lastName: 'Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['loser'],
-    },
-    {
-        key: '3',
-        firstName: 'Joe',
-        lastName: 'Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-    },
-];
+  useEffect(() => {
+    // Gọi API trong useEffect để lấy dữ liệu khi component được tải lần đầu
+    getCustomer();
+  }, []);
+  const getCustomer = () => {
+    setIsLoading(true);
+    ctqmService.customerApi
+      .getAllCustomer()
+      .then((response) => {
+        setCustomers(response);
+      })
+      .catch(({ error }) => {
+        notification.error({
+          message: "An error occurred",
+          description:
+            error?.message ?? "Error in processing, please try again!",
+          placement: "bottomRight",
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-const CustomerManagement: React.FC = () => {
-    useTitle("Customer Management");
+  const handleDelete = async (customerId: string) => {
+    modal.confirm({
+      title: "Are you sure you want to delete this customer?",
+      onOk() {
+        ctqmService.customerApi
+          .deleteCustomerWithId(customerId as string)
+          .then(() => {
+            handleDeleteSuccess();
+          })
+          .catch(({ error }) => {
+            handleDeleteFailed(error);
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
+      },
+    });
+  };
 
-return (
+  const handleDeleteSuccess = () => {
+    message.success("Deleted successfully!");
+    getCustomer();
+  };
+
+  const handleDeleteFailed = (error: any) => {
+    showNotificationError(error);
+  };
+
+  return (
     <React.Fragment>
-        <NavBar />
-        <main>
-            <Table dataSource={data}>
-                <ColumnGroup title="Name">
-                    <Column title="First Name" dataIndex="firstName" key="firstName" />
-                    <Column title="Last Name" dataIndex="lastName" key="lastName" />
-                </ColumnGroup>
-                <Column title="Age" dataIndex="age" key="age" />
-                <Column title="Address" dataIndex="address" key="address" />
-                <Column
-                    title="Tags"
-                    dataIndex="tags"
-                    key="tags"
-                    render={(tags: string[]) => (
-                        <>
-                            {tags.map((tag) => (
-                                <Tag color="blue" key={tag}>
-                                    {tag}
-                                </Tag>
-                            ))}
-                        </>
-                    )}
-                />
-                <Column
-                    title="Action"
-                    key="action"
-                    render={(_: any, record: DataType) => (
-                        <Space size="middle">
-                            <a>Edit</a>
-                            <a>Delete</a>
-                        </Space>
-                    )}
-                />
-            </Table>
-        </main>
-        <Footer />
+      <NavBar />
+      <Card title="Customer List">
+        <div className="w-full flex flex-col border-top justify-end items-end">
+          <Table
+            scroll={{
+              x: "200vh",
+              y: 500,
+            }}
+            dataSource={customers}
+            pagination={false}
+            className="w-full"
+            columns={[
+                {
+                  title: "",
+                  width: 50,
+                  dataIndex: "action",
+                  key: "action",
+                  render(customerId: string) {
+                    return (
+                      <Dropdown
+                        placement="bottomRight"
+                        overlay={
+                          <Menu>
+                            <Link to={"/updateCustomer"}>
+                              <Menu.Item>
+                                <div className="flex gap-3">
+                                <EditOutlined rev={undefined} />
+                                <p>Update</p>
+                                </div>
+                              </Menu.Item>
+                            </Link>
+                            <Menu.Item>
+                              <Popconfirm
+                                title="Delete customer?"
+                                description="Are you sure you want to delete this customer?"
+                                onConfirm={() => handleDelete(customerId)}
+                                okText={<span className="text-black">Yes</span>}
+                                cancelText="No"
+                                className="flex justify-start items-center gap-3"
+                              >
+                                <DeleteOutlined rev={undefined} />
+                                <p>Delete</p>
+                              </Popconfirm>
+                            </Menu.Item>
+                          </Menu>
+                        }
+                      >
+                        <Button icon={<EllipsisOutlined rev={undefined} />} />
+                      </Dropdown>
+                    );
+                  },
+                },
+              ].concat(columns as [])}
+            loading={loading}
+          />
+          <Pagination total={data.totalCount} className="m-2" />
+        </div>
+      </Card>
+      <Footer/>
     </React.Fragment>
-);
-};
-
-
-export default CustomerManagement;
+  );
+}
